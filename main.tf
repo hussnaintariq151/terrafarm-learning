0fb7550207b0eae7df44016485ea4f7591e96ee6
Hussnain Tariq <hussnaintariq151@gmail.com>
2025-12-30 16:25:52 -0800

    Deploying a single web server to AWS EC2

--- main.tf
+++ main.tf
@@ -3,11 +3,40 @@ provider "aws" {
     region = "us-east-2"
     }
 
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+    } 
+
+
+
 resource "aws_instance" "example" {
-    ami         = "ami-015627ae848dee040"
-    instance_type = "t2.micro"
+    ami                    = "ami-015627ae848dee040"
+    instance_type          = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install -y python3
+                echo "Hello World!" > index.html
+                nohup python3 -m http.server 8080 &
+                EOF
+    
+    user_data_replace_on_change = true
 
     tags = {
         Name = "example-instance"
     }
 }
+
+    output "public_ip" {
+    value = aws_instance.example.public_ip
+    }
+
